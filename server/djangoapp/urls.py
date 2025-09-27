df12497e943231fcd453a83cfd9c68eb0009b88c
RodrigoTrevmore <sosimousa@gmail.com>
2025-09-26 22:00:45 -0400

    9th Commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,15 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    # # path for registration
-
-    # path for login
+    path(route='get_cars', view=views.get_cars, name='getcars'),
+    path('logout', views.logout_user, name='logout'),
     path(route='login', view=views.login_user, name='login'),
-
+    path(route='register', view=views.registration, name='registration'),
     # path for dealer reviews view
 
     # path for add a review view
