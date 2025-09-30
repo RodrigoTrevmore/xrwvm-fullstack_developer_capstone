be1704140920aac0403e16d9668005045848bf48
Rodrigo Trevino <sosimousa@gmail.com>
2025-09-30 11:47:37 -0700

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,5 +1,6 @@
 from .models import CarMake, CarModel
 
+
 def initiate():
     car_make_data = [
         {"name": "NISSAN", "description": "Great cars. Japanese technology"},
@@ -11,8 +12,9 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'],
-                                                         description=data['description']))
+        car_make_instances.append(
+            CarMake.objects.create(name=data['name'],
+                                   description=data['description']))
 
     car_model_data = [
       {"name": "Pathfinder", "type": "SUV", "year": 2023,
