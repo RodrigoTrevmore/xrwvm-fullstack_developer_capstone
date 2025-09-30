e123cdfb91c48b918581389fe9031037a6e66e1d
Rodrigo Trevino <sosimousa@gmail.com>
2025-09-30 10:43:36 -0700

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -26,6 +26,6 @@ class CarModel(models.Model):
                                          MaxValueValidator(2023),
                                          MinValueValidator(2015)
                                          ])
-    
+
     def __str__(self):
         return self.model_name
